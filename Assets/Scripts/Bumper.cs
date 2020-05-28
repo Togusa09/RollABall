be91127a54d06fd16edf9bda13a3f801f6b7f8fa
Ben Hockley <ben.hockley@readify.net>
2020-05-28 15:02:05 +1000

    Added bumpers

---
+++ Assets/Scripts/Bumper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("Bumper activated");
+
+        var animator = GetComponentInChildren<Animator>();
+        animator.Play("Bumper");
+    }
+}
