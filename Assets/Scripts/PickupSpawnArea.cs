5b1827a0781069265e99e42eaf77dd4a1914c126
Ben Hockley <ben.hockley@readify.net>
2020-06-04 22:21:38 +1000

    Added random pickup spawning

---
+++ Assets/Scripts/PickupSpawnArea.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawnArea : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(0, 1, 0, 1);
+        Gizmos.DrawWireCube(transform.position, transform.localScale);
+    }
+}
---
+++ Assets/Scripts/PickupSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawner : MonoBehaviour
+{
+    private PickupSpawnArea[] _pickupSpawnAreas;
+
+    public float PickupSpawnInterval;
+    public Pickup PickupPrefab;
+
+    void Awake()
+    {
+        _pickupSpawnAreas = GetComponentsInChildren<PickupSpawnArea>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnPickup());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    IEnumerator SpawnPickup()
+    {
+        while (enabled)
+        {
+            var spawnArea = _pickupSpawnAreas[0]; // Only have one at the moment
+            var maxBound = spawnArea.transform.TransformPoint(new Vector3(0.5f, 0.5f, 0.5f));
+            var minBound = spawnArea.transform.TransformPoint(new Vector3(-0.5f, -0.5f, -0.5f));
+
+            var xSpawnPosition = Random.Range(minBound.x, maxBound.x);
+            var ySpawnPosition = 0.5f; // Shift half unit up as pickup origin is in center
+            var zSpawnPosition = Random.Range(minBound.z, maxBound.z);
+
+            var newPickup = Instantiate(PickupPrefab, new Vector3(xSpawnPosition, ySpawnPosition, zSpawnPosition), Quaternion.identity, transform);
+
+            yield return new WaitForSeconds(PickupSpawnInterval);
+        }
+    }
+}
